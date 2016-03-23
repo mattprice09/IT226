1d4ebd5fc9f8be09e0f2532a85786c020579b9dd
Price, Matt <mrprice@ilstu.edu>
2016-03-22 21:57:25 -0500

    Basic Format

--- src/Asg2/Alarm.java
+++ src/Asg2/Alarm.java
@@ -1,5 +1,51 @@
 package Asg2;
 
-public class Alarm {
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class Alarm {
+	private String message;
+	private LocalDateTime stopTime;
+	private int numSnoozes;
+	
+	public void startTimer(int time) {
+		Timer timer = new Timer();
+		int inSec = time * 1000;
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				triggerAlarm();
+			}
+		}, inSec);
+	}
+	
+	// Matt
+	public static void triggerAlarm() {
+		
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public LocalDateTime getStopTime() {
+		return stopTime;
+	}
+
+	public void setStopTime(LocalDateTime stopTime) {
+		this.stopTime = stopTime;
+	}
+
+	public int getNumSnoozes() {
+		return numSnoozes;
+	}
+
+	public void setNumSnoozes(int numSnoozes) {
+		this.numSnoozes = numSnoozes;
+	}
 }
--- src/Asg2/Main.java
+++ src/Asg2/Main.java
@@ -10,56 +10,53 @@ public class Main {
 
 	public ArrayList<Alarm> alarms = new ArrayList<Alarm>();
 
-	// read file, add alarms to arraylist
+	// Kevin?
 	public static void initiateAll() {
 
 	}
 
+	// Matt
 	public static void displayUI() {
 
 	}
 
-	private static void triggerAlarm() {
+	// Kevin
+	public static void addAlarm(LocalDateTime endDT) {
 
 	}
 
-	public static void addAlarm() {
-
-	}
-
+	// Randy
 	private static void writeAlarm() {
 
 	}
 
+	// Randy
 	private static void writeAll() {
 
 	}
 
+	// Kevin
 	public static void removeAlarm() {
 
 	}
 
 	public static void main(String[] args) {
-		// LocalDate now = LocalDate.now();
-		// LocalDateTime dtnow = LocalDateTime.now();
-		//
-		// LocalDateTime then = dtnow.plusMinutes(30);
-		// if (dtnow.isAfter(then)) {
-		// System.out.println("is after");
-		// } else {
-		// System.out.println("is before");
-		// }
-		// System.out.println(then);
+		 LocalDate now = LocalDate.now();
+		 LocalDateTime dtnow = LocalDateTime.now();
+		
+		 LocalDateTime then = dtnow.plusMinutes(30);
+		 if (dtnow.isAfter(then)) {
+		 System.out.println("is after");
+		 } else {
+		 System.out.println("is before");
+		 }
+		 System.out.println(then);
 
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				triggerAlarm();
-			}
-		}, 500);
+//		Timer timer = new Timer();
+//		Alarm alarm = new Alarm();
+//		alarm.startTimer(2);
 
-		initiateAll();
-		displayUI();
+//		initiateAll();
+//		displayUI();
 	}
 }
